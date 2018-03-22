b9e48432570d9028f5b83968821669392ad4d978
Robert LeCheminant <lecheminant@gmail.com>
2018-03-22 01:10:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays() {
+  return ("Happy holidays!")
+}
+
+function happyHolidaysTo(holiday, name) {
+  holiday = "Easter"
+  name = "Percie"
+  return ('Happy ${holiday}, ${name}!')
+}
+
+function holidayCountdown(days, holiday) {
+  days = "11"
+  holiday = "Easter"
+  return ('It\'s ${days} until ${holiday}!')
+}
